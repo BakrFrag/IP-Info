f300e269d7b017ea6ccddc2b53c513c0aedbe41f
bk <bakr.frag@gmail.com>
2024-10-06 15:33:02 +0300

    include using task id

--- ip_info/ip/consumers.py
+++ ip_info/ip/consumers.py
@@ -29,57 +29,6 @@ class IPConsumer(AsyncWebsocketConsumer):
     async def disconnect(self):
         logger.debug(f"web socket disconnect from {self.scope['client']}")
         pass
-    
-    # async def get_ip_data(self, ip: str) -> Dict[str, str]:
-    #     """
-    #     communicate with ip inf and get ip data
-    #     Parameters:
-    #         ip: str 
-    #             ip to collect data about it
-    #     Raises:
-    #         httpx: httperr exception when connection error happens
-        
-    #     Returns:
-    #         ip info: Dict[str, str]
-    #             collect info about parsed ip 
-    #     """
-    #     try:
-    #         logger.debug(f"receive request to get info from ip {ip}")
-    #         url = f"https://ipinfo.io/{ip}/json"
-    #         async with httpx.AsyncClient() as client:
-    #                 response = await client.get(url)
-    #                 response.raise_for_status()
-    #                 ip_data = response.json()
-    #                 logger.info(f"info collected for ip {ip} with data as {ip_data}")
-    #                 return {"ip": ip, "data": ip_data}
-                
-    #     except httpx.HTTPStatusError as exc:
-
-    #         status_code = exc.response.status_code
-    #         exc_error = exc.response.json().get("error")
-    #         logger.error(f"collect info for ip {ip} failed with status code {status_code} with error {exc_error}")
-    #         return {"ip": ip, "error": exc_error}
-        
-    #     except (httpx.ConnectError, httpx.TimeoutError) as exc:
-    #         logger.error(f"connection error while collect data for ip {ip} with {str(exc)}")
-    #         return {"ip": ip, "error": str(exc)}
-        
-    #     except httpx.HTTPError as exc:
-    #         logger.error(f"error collect ip {ip} data as {str(exc)}")
-    #         return {"ip": ip, "error": str(exc)}
-
-    @staticmethod
-    def task_callback(result, channel_name):
-        """
-        Callback method called when the Celery task is finished.
-        Sends the result back to the WebSocket client via the Channels layer.
-        """
-        channel_layer = get_channel_layer()
-        result_data = result.get()
-        async_to_sync(channel_layer.send)(channel_name, {
-            "type": "send_ip_info",
-            "message": json.dumps(result_data)
-        })
 
     async def send_ip_info(self, event):
         """
