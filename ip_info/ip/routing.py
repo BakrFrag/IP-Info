9ec896a999130c89e25f5e7ced758459dbeb4157
bk <bakr.frag@gmail.com>
2024-10-06 22:57:59 +0300

    use trainling ip

--- ip_info/ip/routing.py
+++ ip_info/ip/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from ip.consumers import IPConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/echo/$', IPConsumer.as_asgi()),
+    re_path(r'ws/ip/$', IPConsumer.as_asgi()),
 ]
